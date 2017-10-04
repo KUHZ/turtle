c6089fbd20ba5b5b863dec2c3638e8946d6a47db
GHZ <kuhzcn@gmail.com>
2017-10-04 10:38:42 +0800

    use loop to draw square

--- draw_square.py
+++ draw_square.py
@@ -1,23 +1,31 @@
 import turtle
 
-def draw_square():
+#def draw_square():
+def draw():
     window = turtle.Screen()
     window.bgcolor("green")
 
+    count = 0
     t1 = turtle.Turtle()
     t1.shape("turtle")
     t1.color("yellow","red")#yellow is the border,red is inside
     t1.speed(2)
     
+    '''
+    t1.forward(100)
+    t1.right(90)
+    t1.forward(100)
+    t1.right(90)   don't use duplicate code
     t1.forward(100)
     t1.right(90)
     t1.forward(100)
     t1.right(90)
-    t1.forward(100)
-    t1.right(90)
-    t1.forward(100)
-    t1.right(90)
-
+    '''
+    while(count<4):
+        t1.forward(100)
+        t1.right(90)
+        count = count + 1
+    
     t2 = turtle.Turtle()
     t2.shape("arrow")
     t2.color("blue")
@@ -26,4 +34,4 @@ def draw_square():
 
     window.exitonclick()
 
-draw_square()
+draw()
