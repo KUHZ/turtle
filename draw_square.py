121bc8a1d9671e9ebb7b6278bb906ea4d6f0a151
GHZ <kuhzcn@gmail.com>
2017-10-04 09:53:28 +0800

    inital commit

---
+++ draw_square.py
@@ -0,0 +1,19 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("green")
+
+    t1 = turtle.Turtle()
+    t1.forward(100)
+    t1.right(90)
+    t1.forward(100)
+    t1.right(90)
+    t1.forward(100)
+    t1.right(90)
+    t1.forward(100)
+    t1.right(90)
+
+    window.exitonclick()
+
+draw_square()
