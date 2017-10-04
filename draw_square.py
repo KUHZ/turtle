fc019d30f14df55b56826ead883a3ec32674ab4a
GHZ <kuhzcn@gmail.com>
2017-10-04 11:17:54 +0800

    draw triangle to circle

--- draw_square.py
+++ draw_square.py
@@ -31,31 +31,40 @@ def draw_square(turtle):
 def draw_triangle(turtle):
     count = 0
     while(count<3):
-        turtle.forward(50)
+        turtle.forward(100)
         turtle.right(120)
         count = count +1
 
 def draw():
     window = turtle.Screen()
     window.bgcolor("green")
+    '''
     #draw square
     t1 = turtle.Turtle()
     t1.shape("turtle")
     t1.color("yellow","red")#yellow is the border,red is inside
-    t1.speed(2)
-    draw_square(t1)
+    t1.speed(10)
+    for i in range(1,73):
+         draw_square(t1)
+         t1.right(5)
+    
     #draw circle
     t2 = turtle.Turtle()
     t2.shape("arrow")
     t2.color("blue")
     t2.speed(2)
     t2.circle(100)
+    '''
     #draw triangle
     t3  = turtle.Turtle()
-    t3.shape("arrow")
+    t3.shape("circle")
+    t3.shapesize(0,0,0)
     t3.color("black")
-    draw_triangle(t3)
- 
+    t3.speed(10)
+    for i in range(1,37):
+         draw_triangle(t3)
+         t3.right(10)
+    
     
 
     window.exitonclick()
